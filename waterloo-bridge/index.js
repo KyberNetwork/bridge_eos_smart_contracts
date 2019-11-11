f5864541f8d53940942d98a1f2a53739be7edc31
Tal Baneth <tal@kyber.network>
2019-11-11 11:26:03 +0200

    refine name - verify -> verifyOnLongest

--- waterloo-bridge/index.js
+++ waterloo-bridge/index.js
@@ -1,8 +1,8 @@
 var Init = require('./init');
 var Relay = require('./relay');
-var Verify = require('./verify');
+var VerifyOnLongest = require('./verifyOnLongest');
 
 //merging all functionality 
-var WaterlooBridge = Object.assign(Init, Relay, Verify);
+var WaterlooBridge = Object.assign(Init, Relay, VerifyOnLongest);
 
 module.exports = WaterlooBridge
